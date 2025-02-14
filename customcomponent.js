ea6cf8d62b7a0d09f3400c3e69c0e00551e5284a
Adhyansareen09 <162317692+Adhyansareen09@users.noreply.github.com>
2025-02-13 22:15:36 -0500

    Create customcomponent.js
    done

---
+++ customcomponent.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+
+export function CustomComponent() {
+  return (
+    <Box sx={{ bgcolor: 'green', color: 'white', p: 3, textAlign: 'center' }}>
+      Welcome to My Store!
+    </Box>
+  );
+}
